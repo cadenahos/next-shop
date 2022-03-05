60560410773c9fb3ac96d312b552db74765a8fc1
Juan Pablo Cadena <juanpablocadena@MacBook-Air-de-Juan.local>
2022-03-05 08:16:15 -0500

    [ADD] pwa

--- next.config.js
+++ next.config.js
@@ -1,23 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    dest:'public',
+    register: true,
+    mode: 'production',
+    disable: false,
+  },
   reactStrictMode: true,
   images: {
     domains: ['placeimg.com'],
-  },
-  // env: {
-  //   customKey: 'customValue',
-  // },
-  // basePath: '/dist',
-  // compress:true,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/hola',
-  //       destination: 'https://juanbahos.com',
-  //       permanent: true,
-  //     }
-  //   ]
-  // }
-}
-
-module.exports = nextConfig
+  }
+})
